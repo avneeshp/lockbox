56ac391b2e790431834857bca03483337f2afe48
Avneesh Pratap <fa274@cornell.edu>
2017-12-11 18:08:26 -0800

    Add files via upload

--- main.go
+++ main.go
@@ -3,18 +3,18 @@ package main
 import (
   "bufio"
   "os"
-	"fmt"
-	"log"
-	"math/big"
+  "fmt"
+  "log"
+  "math/big"
   "strconv"
   "strings"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+  "github.com/ethereum/go-ethereum/accounts/abi/bind"
+  "github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
   "github.com/ethereum/go-ethereum/ethclient"
-	"github.com/ethereum/go-ethereum/core"
+  "github.com/ethereum/go-ethereum/core"
   "github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+  "github.com/ethereum/go-ethereum/crypto"
 )
 
 type ContractData struct {
